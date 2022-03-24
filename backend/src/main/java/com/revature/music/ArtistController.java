84638735fa150b221be956199ad642a70aad440c
MehrabRahman <Mehrab.Rahman@Revature.com>
2022-03-24 10:13:40 -0700

    Feature done

--- backend/src/main/java/com/revature/music/ArtistController.java
+++ backend/src/main/java/com/revature/music/ArtistController.java
@@ -1,8 +1,6 @@
 package com.revature.music;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ArtistController {
     private ArtistRepository artistRepository;
 
--- frontend/src/app/artists.service.ts
+++ frontend/src/app/artists.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Artist } from './artists/artist';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistsService {
   getArtists(): Observable<Artist[]> {
-    const artists = of ([
-      {artist_id: 1, name: 'AC/DC'},
-      {artist_id: 2, name: 'Aerosmith'},
-      {artist_id: 3, name: 'Drake'}
-    ])
-    return artists;
+    return this.http.get<Artist[]>('http://localhost:8081/artists')
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
