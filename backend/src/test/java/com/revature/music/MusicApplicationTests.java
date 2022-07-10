4e4e16a11b1d17c819769f77bd96f5a1a81bfcac
khealy5480 <98979697+khealy5480@users.noreply.github.com>
2022-07-10 11:16:02 -0600

    Update MusicApplicationTests.java

--- backend/src/test/java/com/revature/music/MusicApplicationTests.java
+++ backend/src/test/java/com/revature/music/MusicApplicationTests.java
@@ -1,13 +1,13 @@
-package com.revature.music;
+// package com.revature.music;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MusicApplicationTests {
+// @SpringBootTest
+// class MusicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
