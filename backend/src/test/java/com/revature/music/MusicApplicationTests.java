ad463972d2ad4786369394a9a28fd5c85d973bc2
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-07-10 11:49:38 -0600

    manifest in jar. MusicApplicationTests commented.

--- backend/src/test/java/com/revature/music/MusicApplicationTests.java
+++ backend/src/test/java/com/revature/music/MusicApplicationTests.java
@@ -1,13 +1,13 @@
-package com.revature.music;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MusicApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.revature.music;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MusicApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
